f0dee0824263945770a14b6511c3f9454f31671d
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-17 18:02:01 +0800

    Create LowComplex.sh

---
+++ QC/Step2/LowComplex.sh
@@ -0,0 +1,3 @@
+#exclude those presence in low-complexity regions and minor allele count (≥1)
+cd /home1/Huashan1/wbs_data/public_data/Ensemble_low_complexity_region/LCR-hs38.txt
+#把这里面的snp去掉，用plink --keep保留 
