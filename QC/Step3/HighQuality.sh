0eb9254660267eb7eff2a6076c2235d66963ce73
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-17 17:27:55 +0800

    Update HighQuality.sh

--- QC/Step3/HighQuality.sh
+++ QC/Step3/HighQuality.sh
@@ -1,6 +1,6 @@
 ### Extracting high quality variants to calculate PCA values and kinship relatedness
-# maf selection, higher HWE selection, and indepth pairwise selection
+# maf selection, higher HWE selection, higher genotype quality (>99%) and indepth pairwise selection
 for i in {1..22};do
-plink2 --bfile ./QCStep2/WES_qcstep2_chr${i} --maf 0.0001 --hwe 10e-6 --indep-pairwise 200 100 0.1 --make-bed --out ./QCStep3/HQ_chr${i}_v1
+plink2 --bfile ./QCStep2/WES_qcstep2_chr${i} --maf 0.0001 --geno 0.01 --hwe 10e-6 --indep-pairwise 200 100 0.1 --make-bed --out ./QCStep3/HQ_chr${i}_v1
 plink2 --bfile ./QCStep3/HQ_chr${i}_v1 --indep-pairwise 200 100 0.05 --make-bed --out ./QCStep3/HQ_chr${i}_v2
 done
