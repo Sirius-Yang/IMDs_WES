b457fa861cd186bba0472c202f57e33912b856b4
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-17 22:30:45 +0800

    Update GWAS.bash

--- PostAnalysis/GWAS.bash
+++ PostAnalysis/GWAS.bash
@@ -1,3 +1,4 @@
+# This is a example of performing GWAS analysis for PBC
  #!/bin/bash
 #PBS -N task
 #PBS -q workq
@@ -27,20 +28,3 @@ plink2 --bfile /share/inspurStorage/home1/Vinceyang/UKB_gene_v3_imp_qc/UKB_gene_
 done
 wait
 echo 'Congratulations! Your work has finished'
-
-
-for i in {1..22};do
-plink2 --bfile /share/inspurStorage/home1/Vinceyang/UKB_gene_v3_imp_qc/UKB_gene_v3_imp_qc_chr${i} \
---geno 0.05 \
---mind 0.05 \
---glm hide-covar cols=chrom,pos,ref,alt,a1freq,nobs,beta,se,tz,p \
---maf 0.01 \
---hwe 1e-6 \
---vif 1000 \
---pheno /mnt/storage/home1/Huashan2/yl_data/NHB/GWAS_Pheno1.csv \
---covar /home1/Vinceyang/yl_data/GWAS/GWAS_Covar.csv \
---covar-variance-standardize PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
---covar-col-nums 3,4,5,6,7,8,9,10,11,12,13,14 \
---threads 30 \
---out /mnt/storage/home1/Huashan2/yl_data/NHB/GWAS_Pheno1_chr${i} &
-done
