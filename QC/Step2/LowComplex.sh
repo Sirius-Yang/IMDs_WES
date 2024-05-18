52b3c091fc9701a65364ca0817cd7b91d01a4ca9
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-18 22:46:38 +0800

    Update LowComplex.sh

--- QC/Step2/LowComplex.sh
+++ QC/Step2/LowComplex.sh
@@ -1,4 +1,5 @@
 #exclude those presence in low-complexity regions and minor allele count (≥1)
 wget https://github.com/lh3/varcmp/blob/master/scripts/LCR-hs38.bed.gz
-
-#把这里面的snp去掉，用plink --keep保留 
+awk '{print $0,NR}' LCR-hs38.bed > LCR-hs38.txt
+plink \
+--exclude range /home1/Huashan1/wbs_data/public_data/Ensemble_low_complexity_region/LCR-hs38.txt
