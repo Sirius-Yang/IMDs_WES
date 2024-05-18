4c62e50bee3127c5dd17a7aeaf25ca60d2366497
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 01:47:01 +0800

    Update PCA.sh

--- QC/Step4/PCA.sh
+++ QC/Step4/PCA.sh
@@ -1,4 +1,4 @@
-# Calculate PCA after excluding duplicated samples and relative samples using high quality variants
+# Calculate PCA after excluding duplicated samples and relative samples using high quality variants (result: ukb_wes_chr_all_king_sample_qc_final_unrelated.eigenval)
 # norealte_nodup.txt is the combination of IDs after filtering by king.con and ukb_wes_chr_all_king_sample_qc.kin0
 plink2 --bfile ukb_wes_chr_all_king_sample_qc \
 --keep norelate_nodup.txt \
