d0b5b71df03e0692e98a0712ee0ba1815b4cece7
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 06:22:05 +0800

    Update keep_unrelated_2nd.py

--- QC/Step4/keep_unrelated_2nd.py
+++ QC/Step4/keep_unrelated_2nd.py
@@ -3,12 +3,15 @@ import numpy as np
 
 # Using results from Related.sh to further select independent samples
 data = pd.read_csv('~/UKB_WES_data/qcstep4/ukb_wes_chr_all_king_sample_qc.kin0', delimiter='\t')
+
 data.rename(columns={data.columns[1]: "ID1", data.columns[3]: "ID2", data.columns[7]: "Kinship"}, inplace=True)
+
 data = data[data['Kinship'] >= 0.0884]
 
 # Randomly exclude ID with the largest number of relatives until every ID has only one pair of relative
 continue_signal = True
 while_cnt = 1
+
 while continue_signal and len(data) > 0:
     print(while_cnt)
     while_cnt += 1
@@ -22,7 +25,7 @@ while continue_signal and len(data) > 0:
 
     if sum_df['Freq'].eq(1).all():
         continue_signal = False
-        break
+        break   
     else:
         freq_max = sum_df['Freq'].max()
         sum_max = sum_df[sum_df['Freq'] == freq_max]
@@ -41,6 +44,7 @@ for i in range(len(data_only_id)):
 # Get all of the IDs that has relative relationships and save the results to further excluded by plink
 data_id = pd.read_csv('~/UKB_WES_data/qcstep4/ukb_wes_chr_all_king_sample_qc.kin0', delimiter='\t')
 data_id = data_id[data_id['KINSHIP'] >= 0.0884]
+
 data_id_all = pd.unique(pd.concat([data_id['IID1'], data_id['IID2']]))
 related_sample_remove = np.setdiff1d(data_id_all, data_only_remove[0])
 
