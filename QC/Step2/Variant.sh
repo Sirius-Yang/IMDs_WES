f5d649324a05cd2e66032152ba5abf23b7264eff
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-18 22:59:40 +0800

    Update Variant.sh

--- QC/Step2/Variant.sh
+++ QC/Step2/Variant.sh
@@ -2,5 +2,5 @@
 ls | grep "ukb_merged_chr" > merged.txt # the name of merged files for chr1-22
 # excluding those genotype < 0.1 & HWE p-value < 1E-15
 while read lines; do
-     plink2 --bfile ./QCStep1/${lines} --geno 0.1 --hwe 10e-15 --make-bed --out ./QCStep2/WES_qcstep2_chr${i}
+     plink2 --bfile ./QCStep1/${lines} --geno 0.1 --hwe 10e-15 --make-bed --out ./QCStep2/WES_step2v1_chr${i}
 done < merged.txt
