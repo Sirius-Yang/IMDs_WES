66d6cd0f2e794aee2b26ef0dbe8991e7f2d49082
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 01:11:20 +0800

    Update KeepID.sh

--- QC/Step3/KeepID.sh
+++ QC/Step3/KeepID.sh
@@ -1,6 +1,6 @@
 # Keep those IDs that passed Sample QC using results from Calculate.R
 for i in {1..22};do
-nohup plink --bfile ./QCStep3/WES_step3v1_chr${i} \
+nohup plink --bfile ./QCStep2/WES_step2v2_chr${i} \
 --keep sample_qc_final_keep.txt \
 --make-bed \
 --out ./QCStep3/WES_step3v2_chr${i} &
