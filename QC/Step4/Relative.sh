8bce69d838d1c2d31747181ada69eceef2e34a9e
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 01:28:18 +0800

    Update and rename PCA_Relative.sh to Relative.sh

--- QC/Step4/Relative.sh
+++ QC/Step4/Relative.sh
@@ -1,4 +1,4 @@
-# king matrix of 2nd degree
+# Create king matrix of 2nd degree (ukb_wes_chr_all_king_sample_qc.kin0)
 home1/Huashan1/wbs_data/software/plink2 \
 --bfile /home1/Huashan1/UKB_WES_data/qcstep4/sample_qc_final/ukb_wes_chr_all_king_sample_qc \
 --make-king \ 
