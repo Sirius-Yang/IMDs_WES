881a1d591709db35f64ec83808bbb87ccde84b25
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 01:41:58 +0800

    Update PCA.sh

--- QC/Step4/PCA.sh
+++ QC/Step4/PCA.sh
@@ -1,3 +1,8 @@
-plink --bfile ukb_wes_chr_all_king_sample_qc \
+plink2 --bfile ukb_wes_chr_all_king_sample_qc \
+--exclude \
+--make-bed \
+--out ukb_wes_chr_all_king_sample_qc_final_unrelated
+
+plink2 --bfile ukb_wes_chr_all_king_sample_qc_final_unrelated \
 --pca approx \
 --out ukb_wes_chr_all_king_sample_qc_final_unrelated
