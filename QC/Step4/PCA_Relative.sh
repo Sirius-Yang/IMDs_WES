21cf30c7d0ae41d40a1c7c27e9c33524b760190e
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 01:25:30 +0800

    Update Relative.sh

--- QC/Step4/PCA_Relative.sh
+++ QC/Step4/PCA_Relative.sh
@@ -1,9 +1,8 @@
-#PCA based on king matrix
+# king matrix of 2nd degree
 home1/Huashan1/wbs_data/software/plink2 \
 --bfile /home1/Huashan1/UKB_WES_data/qcstep4/sample_qc_final/ukb_wes_chr_all_king_sample_qc \
 --make-king \ 
 --make-king-table \
 --king-table-filter 0.0884 \
---pca approx \
 --threads 60 \
 --out /home1/Huashan1/UKB_WES_data/qcstep4/ukb_wes_chr_all_king_sample_qc
