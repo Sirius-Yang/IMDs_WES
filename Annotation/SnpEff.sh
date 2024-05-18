b3bf9e219a9b161206a71d4ffb5b301d605748b4
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 02:01:42 +0800

    Update SnpEff.sh

--- Annotation/SnpEff.sh
+++ Annotation/SnpEff.sh
@@ -1,11 +1,11 @@
-### Annotating mutation types and select loss-of-function and missense variants
+### Annotating mutation types to select putative loss-of-function variants
 # snpEff GRCh38.105
 for i in {1..22};
 do
 java -jar snpEff.jar  GRCh38.105 /home1/Huashan1/UKB_WES_data/snpEff/input/ukb_wes_chr${i}_qc.bim > /home1/Huashan1/UKB_WES_data/snpEff/output/GRCh38.105/ukb_wes_chr${i}_qc.vcf
 done
 
-### Further annotating missense to define putative damaging missense variants
+### Annotating using 5 methods to define putative damaging missense variants
 # snpEff dbnsfp
 for i in {1..22};
 do
