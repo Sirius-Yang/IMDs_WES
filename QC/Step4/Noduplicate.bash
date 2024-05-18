5875bb663b82a32a8c1e6874861a6829ac39ddd9
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 01:29:05 +0800

    Update Noduplicate.bash

--- QC/Step4/Noduplicate.bash
+++ QC/Step4/Noduplicate.bash
@@ -1,3 +1,3 @@
-## exclude duplicated
+## exclude duplicated (result: king.con)
 cd /home1/Huashan1/UKB_WES_data/qcstep4/
 king -b /home1/Huashan1/UKB_WES_data/qcstep4/HQ_autosomal_gene/ukb_wes_chr_all_king_new.bed --duplicate --cpus 64
