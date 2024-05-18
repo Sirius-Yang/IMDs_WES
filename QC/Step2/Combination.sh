262ae7df2c21cf47d6c56c464e10a66467e90db9
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-18 23:36:43 +0800

    Update Combination.sh

--- QC/Step2/Combination.sh
+++ QC/Step2/Combination.sh
@@ -5,10 +5,6 @@ mkdir ~/UKB_WES_data/qcstep1/merge_by_chrom/
 cd ~/UKB_WES_data/qcstep1/merge_by_chrom/
 dx download project-GF4jpV8JyFyK68BP64YpqFYQ:/Sirius_data/plink/ukb*
 
-for i in $chrt; do
-  cat merge_list_chr${i}.txt | head -n 1 >> merge_list_first_row_clean.txt
-done
-
 # Combination files 
 n=0
 while IFS= read -r line;do
