2d3cdaa54e6426f95c66655d3d05bf865b558047
Sirius-Yang <74815540+Sirius-Yang@users.noreply.github.com>
2024-05-19 19:18:18 +0800

    Update SnpEff.sh

--- Annotation/SnpEff/SnpEff.sh
+++ Annotation/SnpEff/SnpEff.sh
@@ -2,12 +2,17 @@
 # snpEff GRCh38.105
 for i in {1..22};
 do
-java -jar snpEff.jar  GRCh38.105 /home1/Huashan1/UKB_WES_data/snpEff/input/ukb_wes_chr${i}_qc.bim > /home1/Huashan1/UKB_WES_data/snpEff/output/GRCh38.105/ukb_wes_chr${i}_qc.vcf
+java -jar snpEff.jar  GRCh38.105 /home1/Huashan1/UKB_WES_data/snpEff/input/ukb_wes_chr${i}_qc.bim \
+> /home1/Huashan1/UKB_WES_data/snpEff/output/GRCh38.105/ukb_wes_chr${i}_SnpEff.vcf
 done
 
 ### Annotating using 5 methods to define putative damaging missense variants
 # snpEff dbnsfp
 for i in {1..22};
 do
-java -jar SnpSift.jar dbnsfp -f genename,Ensembl_geneid,Uniprot_acc,LRT_pred,Polyphen2_HDIV_pred,MutationTaster_pred,Polyphen2_HVAR_pred,SIFT_pred -db /home1/Huashan1/wbs_data/software/snpEff/data/dbNSFP/dbNSFP.txt.gz -g hg38 -v /home1/Huashan1/UKB_WES_data/snpEff/input/ukb_wes_chr${i}_qc.bim > /home1/Huashan1/UKB_WES_data/snpEff/output/dbnsfp/five/ukb_wes_chr${i}_qc_five.vcf
+java -jar SnpSift.jar dbnsfp \
+-f genename,Ensembl_geneid,Uniprot_acc,LRT_pred,Polyphen2_HDIV_pred,MutationTaster_pred,Polyphen2_HVAR_pred,SIFT_pred \
+-db /home1/Huashan1/wbs_data/software/snpEff/data/dbNSFP/dbNSFP.txt.gz \
+-g hg38 -v /home1/Huashan1/UKB_WES_data/snpEff/input/ukb_wes_chr${i}_qc.bim \
+> /home1/Huashan1/UKB_WES_data/snpEff/output/dbnsfp/five/ukb_wes_chr${i}_SnpEff_five.vcf
 done
